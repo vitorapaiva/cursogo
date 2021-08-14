1015d188116d42554dc52856a9b470396b32cd4c
Vitor Paiva <vitorapaiva@gmail.com>
2021-08-14 02:10:14 +0000

    Initial commit

---
+++ exercicio/Exercicio.go
@@ -0,0 +1,24 @@
+package exercicio
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func Exercicio1() {
+  fmt.Println("Exercicio 1")
+  x:=42
+  y:="James Bond"
+  z:=true
+  fmt.Println(x, y, z)
+  fmt.Println(x)
+  fmt.Println(y)
+  fmt.Println(z)
+}
+
+func Exercicio2() {
+  fmt.Println("Exercicio 2")
+  fmt.Println(x, y, z)
+  //valor zero
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "main/exercicio"
+
+func main() {
+  exercicio.Exercicio1()
+  exercicio.Exercicio2()
+}
