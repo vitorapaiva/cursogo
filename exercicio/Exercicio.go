69ff56b9552a86e0a8da49809629d62b23df2f86
Vitor Paiva <vitorapaiva@gmail.com>
2021-08-31 16:27:49 +0000

    Adicionando novos exercicios

--- exercicio/Exercicio.go
+++ exercicio/Exercicio.go
@@ -6,6 +6,10 @@ var x int
 var y string
 var z bool
 
+var x3 int = 42
+var y3 string = "James Bond"
+var z3 bool = true
+
 func Exercicio1() {
   fmt.Println("Exercicio 1")
   x:=42
@@ -21,4 +25,23 @@ func Exercicio2() {
   fmt.Println("Exercicio 2")
   fmt.Println(x, y, z)
   //valor zero
+}
+
+func Exercicio3() {
+  fmt.Println("Exercicio 3")
+  s := fmt.Sprintf("%v, %v, %v", x3, y3, z3)
+  fmt.Println(s)
+}
+
+func Exercicio4() {
+  fmt.Println("Exercicio 4")
+  
+}
+
+func Exercicio5() {
+  fmt.Println("Exercicio 5")
+}
+
+func Exercicio6() {
+  fmt.Println("Exercicio 6")
 }
--- main.go
+++ main.go
@@ -5,4 +5,8 @@ import "main/exercicio"
 func main() {
   exercicio.Exercicio1()
   exercicio.Exercicio2()
+  exercicio.Exercicio3()
+  exercicio.Exercicio4()
+  exercicio.Exercicio5()
+  exercicio.Exercicio6()
 }
